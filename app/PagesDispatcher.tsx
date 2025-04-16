e858dedc33f63d36e109ef63e8ad00ed28df93fe
zlatandp <zlatandnepr@gmail.com>
2025-04-16 18:50:05 +0300

    changed dispatcher rules

--- app/PagesDispatcher.tsx
+++ app/PagesDispatcher.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { nextStep } from "@/app/actions/steps-client.action";
 import { StepType } from "@/app/actions/actions.types";
 import { getCurrentTime } from "@/app/actions/steps.action";
 import { useState, useEffect } from "react";
 
 export default function PagesDispatcher() {
-  // const router = useRouter();
+  const router = useRouter();
   const searchParams = useSearchParams();
   const [pageEnter, setPageEnter] = useState<boolean>(false);
 
@@ -41,8 +41,7 @@ export default function PagesDispatcher() {
           window.location.href =
             "https://www.figma.com/proto/mLXyPVFWeoms9vfGsoKfld/TMOD---China-Exhibition?page-id=787%3A6127&node-id=743-1666&viewport=7136%2C1703%2C0.28&t=3mb2Jkln4YvumMcL-8&scaling=scale-down&content-scaling=fixed&starting-point-node-id=743%3A1666&hotspot-hints=0&disable-default-keyboard-nav=1&hide-ui=1";
         } else {
-          window.location.href =
-            "https://www.figma.com/proto/mLXyPVFWeoms9vfGsoKfld/TMOD---China-Exhibition?page-id=787%3A6129&node-id=787-1719&viewport=4483%2C936%2C0.17&t=vibmDX4JwVC9W1OO-8&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=787%3A1719&hotspot-hints=0&show-proto-sidebar=1&disable-default-keyboard-nav=1&hide-ui=1";
+          router.push(`/tmod`);
         }
     }
   }, [pageEnter]);
